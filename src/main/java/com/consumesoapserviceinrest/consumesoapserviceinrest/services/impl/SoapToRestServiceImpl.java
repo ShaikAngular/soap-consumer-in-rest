62048b59ccd0fb4d9a9f83daec1b988b3a0aa143
shaik <indivarangular@gmail.com>
2022-10-17 23:19:33 +0530

    consuming SOAP service in REST---second commit--Working fine--done addition, subtraction and multiplication--wip

--- src/main/java/com/consumesoapserviceinrest/consumesoapserviceinrest/services/impl/SoapToRestServiceImpl.java
+++ src/main/java/com/consumesoapserviceinrest/consumesoapserviceinrest/services/impl/SoapToRestServiceImpl.java
@@ -16,7 +16,9 @@ public class SoapToRestServiceImpl implements SoapToRestService {
 	public String getStringFromService() {
 		Calculator calculator = new Calculator();
 		// TODO Auto-generated method stub
-		return "the sum is =" + calculator.getCalculatorSoap().add(5, 7);
+		return "the sum of 7 and 5 is =" + calculator.getCalculatorSoap().add(5, 7) 
+				+ "the difference of 7 and 5 is =" + calculator.getCalculatorSoap().subtract(10,7)
+				+ "the product of 7 and 5 is =" + calculator.getCalculatorSoap().multiply(7, 5);
 	}
 
 }
